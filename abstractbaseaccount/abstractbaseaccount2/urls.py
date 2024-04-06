c58b1e44ea25602fe6dd373b3e48839aa4271652
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-04-06 10:52:59 +0300

    Upd app

--- abstractbaseaccount/abstractbaseaccount2/urls.py
+++ abstractbaseaccount/abstractbaseaccount2/urls.py
@@ -1,4 +1,4 @@
-"""abstractbaseaccount2 URL Configuration
+"""abstractbaseaccount URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- abstractbaseaccount/abstractbaseaccount2/wsgi.py
+++ abstractbaseaccount/abstractbaseaccount2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for abstractbaseaccount2 project.
+WSGI config for abstractbaseaccount project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abstractbaseaccount2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abstractbaseaccount.settings')
 
 application = get_wsgi_application()
